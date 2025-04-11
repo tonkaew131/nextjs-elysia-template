570a918de8574c56ab7bf9a02b6d191e5777e94d
tonkaew131 <nongtonkaew@gmail.com>
2025-04-11 19:23:27 +0700

    Add swagger to api

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,7 +1,11 @@
+import swagger from '@elysiajs/swagger';
 import { Elysia } from 'elysia';
 
-const app = new Elysia().get('/', () => 'Hello Elysia').listen(3001);
+const api = new Elysia()
+    .use(swagger())
+    .get('/', () => 'Hello Elysia')
+    .listen(3001);
 
 console.log(
-    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+    `🦊 Elysia is running at ${api.server?.hostname}:${api.server?.port}`
 );
