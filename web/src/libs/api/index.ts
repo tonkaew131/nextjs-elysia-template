1850032f94e30901df62cff2523ea23ed7b397e1
Athicha L. (mac) <nongtonkaew@gmail.com>
2025-04-18 18:51:17 +0700

    Add credential 'include' to fetchClient

--- web/src/libs/api/index.ts
+++ web/src/libs/api/index.ts
@@ -5,5 +5,6 @@ import type { paths } from './api';
 
 export const fetchClient = createFetchClient<paths>({
     baseUrl: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/',
+    credentials: 'include',
 });
 export const $api = createClient(fetchClient);
