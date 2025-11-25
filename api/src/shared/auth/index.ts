2818791ef09c79cc4503e7af37c3af57e4ad6129
tonkaew131 <nongtonkaew@gmail.com>
2025-11-25 16:48:16 +0700

    Fix better auth missing table schema

--- api/src/shared/auth/index.ts
+++ api/src/shared/auth/index.ts
@@ -2,10 +2,12 @@ import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 
 import { db } from '@api/shared/db';
+import * as schema from '@api/shared/db/schema';
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: 'pg',
+        schema: schema,
     }),
     emailAndPassword: {
         enabled: true,
